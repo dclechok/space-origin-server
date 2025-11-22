6add71e71d10b02e41896a6c886c5841003f2676
DC Lechok <dclechok@gmail.com>
2025-11-21 22:09:09 -0500

    fixing characterController

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,26 +1,50 @@
-const { ObjectId, isValidObjectId } = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 exports.getCharactersForAccount = async (req, res) => {
   try {
-    console.log(req.params)
     const accountId = req.params.id;
 
     if (!ObjectId.isValid(accountId)) {
-      console.log("Invalid ObjectId:", accountId);
+      console.log("Invalid accountId:", accountId);
       return res.json({ characters: [] });
     }
 
     const db = req.app.locals.db;
-    const accounts = db.collection("player_data");
 
-    const account = await accounts.findOne(
+    const usersCol = db.collection("accounts");
+    const charsCol = db.collection("player_data");
+
+    // Find the user account
+    const user = await usersCol.findOne(
       { _id: new ObjectId(accountId) },
       { projection: { passwordHash: 0 } }
     );
 
-    if (!account) return res.json({ characters: [] });
+    if (!user) {
+      console.log("User not found:", accountId);
+      return res.json({ characters: [] });
+    }
 
-    res.json({ characters: account.characters || [] });
+    // User.characters contains the character IDs
+    const characterIds = (user.characters || []).filter(id => id);
+
+    if (characterIds.length === 0) {
+      return res.json({ characters: [] });
+    }
+
+    // Convert all IDs to ObjectId
+    const objectIds = characterIds.map(id => new ObjectId(id));
+
+    // Fetch all character documents from player_data
+    const characters = await charsCol
+      .find(
+        { _id: { $in: objectIds } },
+        { projection: { passwordHash: 0 } }
+      )
+      .toArray();
+
+    // Return them
+    res.json({ characters });
 
   } catch (err) {
     console.error("Character fetch error:", err);
